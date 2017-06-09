ed8b53509277b7c416bfec438a3dca84db666900
JohnBay <moneymashi@gmail.com>
2017-06-09 18:33:19 +0900

    ++ SVN 0509 in class

--- springweb/src/springweb/c01_database/controller/A01_EmpCtrl.java
+++ springweb/src/springweb/c01_database/controller/A01_EmpCtrl.java
@@ -6,16 +6,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import springweb.c01_database.service.A01_EmpService;
+import springweb.z01_dto.Emp;
 
 @Controller
 public class A01_EmpCtrl {
 //	
 	@Autowired(required=false)
 	private A01_EmpService service;	
+	
+//	http://localhost:6080/springweb/emplist_my.do
 	@RequestMapping("/emplist_my.do")
-	public String empList(Model d){
+	public String empList(Emp sch, Model d){
+
 		
-		d.addAttribute("emplist", service.empList());
+		d.addAttribute("emplist", service.empList(sch));
 		
 		return "c01_database/a01_empList";
 	}
--- springweb/src/springweb/c01_database/repository/A01_EmpRep.java
+++ springweb/src/springweb/c01_database/repository/A01_EmpRep.java
@@ -11,5 +11,9 @@ public interface A01_EmpRep {
 //	xml에 있는 id값과 다음 같은 규칙으로 mapping
 //	id="패키지명.인터페이스명.메서드명
 //	id="springweb.c01_database.repository.A01_EmpRep.emplist"
-	public ArrayList<Emp> emplist();
+//	전달하는 data있는 VO : Emp sch
+//	parameterType="springweb.z01_dto.Emp"
+//	데이터 결과 처리 ArrayList<VO>
+//	
+	public ArrayList<Emp> emplist(Emp sch);
 }
